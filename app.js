eef892414f27e4e21237a213a7b97c1826fbfa26
Kaustub Navalady <kaustub.nvd@gmail.com>
2020-06-28 00:48:59 -0500

    Create project structure
    Added routing, controllers, view engine, and static folder.

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./routes/home'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ controllers/home.js
@@ -0,0 +1,3 @@
+exports.getHome = (req, res) => {
+  res.render('home');
+};
