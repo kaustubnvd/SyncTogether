adc32853bfa93f6475bb24bcdc76777032af056e
Kaustub Navalady <kaustub.nvd@gmail.com>
2020-07-03 13:48:03 -0500

    Add real-time synced video and controls
    Introduced the core functionality of the application. There still exists some bugs that need to be fixed.

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app); // Need the server for socket-io
-const io = require('socket.io')(server); 
+const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(require('./routes/home'));
 app.use(require('./routes/room'));
@@ -23,18 +23,54 @@ server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 const rooms = require('./models/rooms');
 
 // eslint-disable-next-line no-unused-vars
-io.on('connection', socket => {
-
+io.on('connection', (socket) => {
+  // A user has joined a new room
   socket.on('new-user', (name, room) => {
     socket.join(room);
+    // Map their socket id to their nickname
     rooms[room].users[socket.id] = name;
   });
 
-  socket.on('chat-message', (message, room) => {
-    const nickname = rooms[room].users[socket.id]; // The socket.id maps to the username
-    const msgTime = moment().format('h:mm a'); // When the message was sent 
-    // Emits to all sockets in the room, including the sender
-    io.in(room).emit('chat-message', {name: nickname, message: message, time: msgTime});
+  // A new user's YouTube player has loaded
+  socket.on('loaded', (room) => {
+    const participant = Object.keys(rooms[room].users)[0]; // Gets a user
+    io.to(participant).emit('new-user-loaded', socket.id);
   });
 
-}); 
+  // Send the new user the meta-data of the current video playing
+  socket.on('current-video', (videoUrl, elapsedTime, newUser) => {
+    io.to(newUser).emit('current-video', videoUrl, elapsedTime);
+  });
+
+  // A user resumes the video
+  socket.on('resume', (elapsedTime, room) => {
+    socket.to(room).broadcast.emit('resume', elapsedTime);
+  });
+
+  // A user pauses the video
+  socket.on('pause', (room) => {
+    socket.to(room).broadcast.emit('pause');
+  });
+
+  // Prevents unnecessary events from firing
+  socket.on('calibrate', (room) => {
+    socket.to(room).broadcast.emit('calibrate');
+  });
+
+  // A user loads a new video in the room
+  socket.on('new-video', (videoUrl, room) => {
+    socket.to(room).broadcast.emit('new-video', videoUrl);
+  });
+
+  // A user sends a chat message
+  socket.on('chat-message', (message, room) => {
+    const nickname = rooms[room].users[socket.id]; // The socket.id maps to the username
+    const msgTime = moment().format('h:mm a'); // When the message was sent
+    // Emits to all sockets in the room, including the sender
+    io.in(room).emit('chat-message', {
+      name: nickname,
+      message: message,
+      time: msgTime,
+    });
+  });
+});
--- public/js/main.js
+++ public/js/main.js
@@ -3,42 +3,9 @@ const socket = io(); // Client controller
 
 // eslint-disable-next-line no-undef
 const name = nickname || prompt('Enter a nickname:'); // Ask for name if room is accessed through URL
-// eslint-disable-next-line no-undef 
+// eslint-disable-next-line no-undef
 const room = roomName; // defined in room.ejs
 socket.emit('new-user', name, room);
-
-/* Chat */
-
-const chatArea = document.querySelector('.chat-area');
-const chatInput = document.getElementById('chat-input');
-
-chatInput.addEventListener('keyup', (e) => {
-  if(e.code === 'Enter') {
-    const message = e.target.value;
-    // Prevent empty messages
-    if (message === '') {
-      return;
-    }
-    socket.emit('chat-message', message, room);
-    e.target.value = ''; // Clear user input
-    // Scrolls down
-    chatArea.scrollTop = chatArea.scrollHeight;
-  }
-});
-
-socket.on('chat-message', messageData => {
-  addMessage(messageData.name, messageData.message, messageData.time);
-  // Scrolls down on new message
-  chatArea.scrollTop = chatArea.scrollHeight;
-});
-
-// Creates chat message and adds it to the DOM
-function addMessage(user, message, time) {
-  const messageElem = document.createElement('div');
-  messageElem.classList.add('chat-msg');
-  messageElem.innerHTML = `<p class="meta">${user} <span>${time}</span></p><p class="msg-text">${message}</p>`; // TODO: Fix XSS
-  chatArea.append(messageElem);
-}
 
 /* YouTube player */
 
@@ -51,23 +18,168 @@ function onYouTubeIframeAPIReady() {
     height: '719',
     width: '1283',
     videoId: '',
-    events: {},
+    events: { onReady: onPlayerReady, onStateChange: onPlayerStateChange },
   });
 }
 
 function onPlayerReady(event) {
-  event.target.playVideo();
+  socket.emit('loaded', room);
 }
 
+function onPlayerStateChange(event) {
+  controlVideo(event.data);
+}
+
+// Bugs: Occasional desync issues, pausing/loading a video issues, seeking causes slight jitter
+// Note: Pausing the video seems to sync better than seeking
+// Test: Simultaneous requests and long-distance delay
+
+// When certain socket events are handled, they inadvertently fire the YouTube player's 'onStateChange' events;
+// The following booleans are scattered throughout the code to prevent unnecessary re-emitting of events
+let preventPlay = false; 
+let preventPause = false;
+let preventReseek = false;
+
+const PLAY = 1;
+const LOAD = -1;
+const PAUSE = 2;
+const BUFFER = 3;
+
+/* Events (some still re-emit)
+   First user (host) loaded video: -1 3 1
+   New video load: 2 -1 3 1 
+*/
+
+/* eslint-disable */
+
+// Reacts to YouTube player state changes
+function controlVideo(playerState) {
+  switch (playerState) {
+    case PLAY:
+      if (!preventPlay) {
+        socket.emit('resume', player.getCurrentTime(), room);
+      }
+      // Reset values for next request
+      preventPause = false;
+      preventPlay = false;
+      break;
+    case PAUSE:
+      if (!preventPause) {
+        socket.emit('pause', room);
+      }
+      // Reset values
+      preventPause = false;
+      if (!preventReseek) {
+        preventPlay = false;
+      }
+      break;
+    case BUFFER:
+     preventReseek = true; // Seeking involves pause -> buffer -> play; the play event induced from seeking shouldn't refire
+     break;
+  }
+}
+/* eslint-enable */
+
+let userPause = true;
 const search = document.getElementById('searchbar');
 search.addEventListener('keyup', (e) => {
-  if(e.code === 'Enter') {
+  if (e.code === 'Enter') {
     // YouTube URL in the search bar
     if (!e.target.value.startsWith('https')) {
       return;
     }
     const url = new URL(e.target.value);
+    userPause = false; // Edge Case: When a new video is loaded, it first pauses (2), causing unncessary events to fire; this boolean prevents 'pause' from being emitted by the video load
+    preventPlay = true; // When the other users load the video, they will automatically play
+    preventPause = true;  // So it's not required to emit a play/pause event
+    socket.emit('calibrate', room);
     // Loads a new video, with the video id extracted from the query params
     player.loadVideoById(url.searchParams.get('v'), 0, 'large');
+    setTimeout(() => socket.emit('new-video', url, room), 1000); // Doesn't run synchronously at times, so slight delay necessary
+    userPause = true; // Edge case: The first video loaded does NOT pause as the first step, so this line reverts it
   }
 });
+
+// Sends the new user the meta-info of the video currently playing in the room
+socket.on('new-user-loaded', (newUser) => {
+  const videoUrl = player.getVideoUrl();
+  const elapsedTime = player.getCurrentTime();
+  socket.emit('current-video', videoUrl, elapsedTime, newUser);
+});
+
+// A new users loads the currently playing video in the room
+socket.on('current-video', (videoUrl, elapsedTime) => {
+  preventPlay = true;
+  const url = new URL(videoUrl);
+  player.loadVideoById(url.searchParams.get('v'), elapsedTime, 'large');
+  setTimeout(() => socket.emit('resume', elapsedTime, room), 1000); // Waits for new user's video to load before resuming
+});
+
+// A user resumed playback
+socket.on('resume', (elapsedTime) => {
+  preventPlay = true; // Prevent re-emit
+  player.seekTo(elapsedTime);
+  player.playVideo();
+});
+
+// A user paused the video
+socket.on('pause', () => {
+  preventPause = true; // Prevent re-emit
+  if (userPause) {
+    player.pauseVideo();
+  }
+  userPause = true;
+});
+
+// Prevents the 'pause' event from firing on a new video laod
+socket.on('calibrate', () => {
+  userPause = false;
+  preventPause = true;
+});
+
+// A user loaded a new video in the room
+socket.on('new-video', (videoUrl) => {
+
+  // Prevent re-emit
+  preventReseek = true;
+  preventPlay = true; 
+  preventPause = true;
+
+  const url = new URL(videoUrl);
+  player.loadVideoById(url.searchParams.get('v'), 0, 'large');
+  userPause = true;
+
+});
+
+/* Chat */
+
+const chatArea = document.querySelector('.chat-area');
+const chatInput = document.getElementById('chat-input');
+
+chatInput.addEventListener('keyup', (e) => {
+  if (e.code === 'Enter') {
+    const message = e.target.value;
+    // Prevent empty messages
+    if (message === '') {
+      return;
+    }
+    socket.emit('chat-message', message, room);
+    e.target.value = ''; // Clear user input
+    // Scrolls down
+    chatArea.scrollTop = chatArea.scrollHeight;
+  }
+});
+
+socket.on('chat-message', (messageData) => {
+  addMessage(messageData.name, messageData.message, messageData.time);
+  // Scrolls down on new message
+  chatArea.scrollTop = chatArea.scrollHeight;
+});
+
+// Creates chat message and adds it to the DOM
+function addMessage(user, message, time) {
+  const messageElem = document.createElement('div');
+  messageElem.classList.add('chat-msg');
+  messageElem.innerHTML = `<p class="meta">${user} <span>${time}</span></p><p class="msg-text">${message}</p>`; // TODO: Fix XSS
+  chatArea.append(messageElem);
+}
